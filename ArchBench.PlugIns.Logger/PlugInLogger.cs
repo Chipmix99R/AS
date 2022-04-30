26cf13a95f77853ec62dc15d34da6ec6dfe950db
Chipmix99R <95168359+Chipmix99R@users.noreply.github.com>
2022-04-30 17:58:37 +0100

    Add files via upload

---
+++ ArchBench.PlugIns.Logger/PlugInLogger.cs
@@ -0,0 +1,42 @@
+﻿using HttpServer;
+using HttpServer.Sessions;
+
+namespace ArchBench.PlugIns.Logger
+{
+    public class PlugInLogger : IArchBenchHttpPlugIn
+    {
+        public string Name        => "Logger";
+        public string Description => "Log all requests";
+        public string Author      => "Leonel Nóbrega";
+        public string Version     => "1.0";
+
+        public bool Enabled { get; set; }
+        public IArchBenchPlugInHost Host { get; set; }
+
+        public IArchBenchSettings Settings { get; } = new ArchBenchSettings();
+
+        public void Dispose()
+        {
+        }
+
+        public void Initialize()
+        {
+            Settings[ "Headers" ] = false.ToString();
+        }
+
+        public bool Process(IHttpRequest aRequest, IHttpResponse aResponse, IHttpSession aSession)
+        {
+            Host.Logger.WriteLine("{0,8} {1} : {2}", aRequest.Method, aRequest.HttpVersion, aRequest.Uri.AbsoluteUri );
+
+            if ( ! bool.TryParse( Settings[ "Headers" ], out var headers ) ) return false;
+            if ( ! headers ) return false;
+
+            foreach (var key in aRequest.Headers.AllKeys)
+            {
+                Host.Logger.WriteLine("{0}: {1}", key, aRequest.Headers[key]);
+            }
+
+            return false;
+        }
+    }
+}
