19f70b3fc53a8af29b07487b21bba3c134a6099b
DerfStein <37420049+DerfStein@users.noreply.github.com>
2022-05-02 06:56:59 +0100

    implementação do throttling

---
+++ ArchBench.PlugIns.Proxy/PlugInProxy.cs
@@ -0,0 +1,89 @@
+﻿using HttpServer;
+using HttpServer.Sessions;
+using System;
+using System.Collections.Specialized;
+using System.Net;
+
+namespace ArchBench.PlugIns.Proxy
+{
+    public class PlugInProxy : IArchBenchHttpPlugIn
+    {
+        public string Name => @"ArchBench Proxy PlugIn";
+
+        public string Description => @"Implements a HTTP Proxy";
+
+        public string Author => @"Leonel Nobrega";
+
+        public string Version => @"1.0";
+
+        public bool Enabled { get; set; }
+        public IArchBenchPlugInHost Host { get; set; }
+
+        public IArchBenchSettings Settings { get; } = new ArchBenchSettings();
+
+        public void Dispose()
+        {
+        }
+
+        public void Initialize()
+        {
+            Settings["Remote"] = "http://localhost:8082";
+        }
+
+        public bool Process(IHttpRequest aRequest, IHttpResponse aResponse, IHttpSession aSession)
+        {
+            var url = $"{Settings["Remote"]}{aRequest.Uri.AbsolutePath}";
+
+            try
+            {
+                var client = new WebClient();
+                
+                ForwardCookies( client, aRequest );
+
+                byte[] bytes = null;
+
+                if (aRequest.Method == Method.Post)
+                {
+                    var inputs = ForwardInputs(aRequest);
+                    bytes = client.UploadValues(url, inputs);
+                }
+                else
+                {
+                    bytes = client.DownloadData(url);
+                }
+
+                BackwardCookies(client, aResponse);
+
+                aResponse.Body.Write(bytes, 0, bytes.Length);
+            }
+            catch (Exception e)
+            {
+                Host.Logger.WriteLine($"Error: {e.Message} on getting '{url}'");
+                return false;
+            }
+            return true;
+        }
+        private void ForwardCookies(WebClient client, IHttpRequest request)
+        {
+            if (request.Headers["Cookie"] == null) return;
+            
+            client.Headers.Add("Cookie", request.Headers["Cookie"]);
+        }
+        private void BackwardCookies( WebClient client, IHttpResponse response )
+        {
+            if (client.ResponseHeaders["Set-Cookie"] == null) return;
+
+            response.AddHeader("Set-Cookie", client.ResponseHeaders["Set-Cookie"]);
+        }
+
+        private NameValueCollection ForwardInputs(IHttpRequest request)
+        {
+            var inputs = new NameValueCollection();
+            foreach (HttpInputItem item in request.Form)
+            {
+                inputs.Add(item.Name, item.Value);
+            }
+            return inputs;
+        }
+    }
+}
---
+++ ArchBench.PlugIns.Throttling/PlugInThrottling.cs
@@ -0,0 +1,98 @@
+﻿using HttpServer;
+using HttpServer.Sessions;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ArchBench.PlugIns.Throttling
+{
+    public class PlugInThrottling : IArchBenchHttpPlugIn
+    {
+        public string Name => "ArchBench Hello Throttling";
+        public string Description => "Este plugin faz o Throttling do recurso hello usando o SemaphoreSlim";
+        public string Author => "Duarte Santos";
+        public string Version => "1.0";
+
+
+        SemaphoreSlim gate = new SemaphoreSlim(1);
+
+
+        public bool Enabled { get; set; } = false;
+
+        
+
+        public IArchBenchPlugInHost Host { get; set; }
+        public IArchBenchSettings Settings { get; } = new ArchBenchSettings();
+
+        public void Dispose()
+        {
+        }
+
+        public void Initialize()
+        {
+            
+        }
+
+
+        public  bool Process(IHttpRequest aRequest, IHttpResponse aResponse, IHttpSession aSession)
+        {
+            if (aRequest.Uri.AbsolutePath.StartsWith("/hello", StringComparison.InvariantCultureIgnoreCase))
+            {
+                try
+                {
+                    await gate.WaitAsync();
+                    aSession["Count"] = (int?)aSession["Count"] + 1 ?? 1;
+                    var ordinal = AddOrdinal((int)aSession["Count"]);
+
+                    var writer = new StreamWriter(aResponse.Body);
+                    writer.WriteLine($"Hi! for the { ordinal } time.");
+                    writer.Flush();
+
+                    Host.Logger.WriteLine($"Saying Hello for the { ordinal } times.");
+                    gate.Release();
+                }
+                catch(Exception ex)
+                {
+                    Host.Logger.WriteLine(ex.ToString());
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+        //Hello com o throttle SemaphoreSlim
+        
+            
+        
+
+        public static string AddOrdinal(int num)
+        {
+            if (num <= 0) return num.ToString();
+
+            switch (num % 100)
+            {
+                case 11:
+                case 12:
+                case 13:
+                    return num + "th";
+            }
+
+            switch (num % 10)
+            {
+                case 1:
+                    return num + "st";
+                case 2:
+                    return num + "nd";
+                case 3:
+                    return num + "rd";
+                default:
+                    return num + "th";
+            }
+        }
+
+
+    }
+   
+
+}
